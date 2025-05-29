fc9d5339532e867f96098eac81270623f8cd7f07
Renan Matthew Fajardo <renanmatthew206@gmail.com>
2025-05-29 23:39:33 +0800

    chore: initialize tailwind and create header component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </nav>
+  <HeaderNav />
   <router-view />
 </template>
 
-<script></script>
+<script setup>
+import HeaderNav from "./components/HeaderNav.vue";
+</script>
---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="flex gap-8 justify-center">
+    <router-link to="/" class="underline">Home</router-link>
+    <router-link to="/about" class="underline">About</router-link>
+  </nav>
+</template>
+
+<script></script>
+
+<style></style>
